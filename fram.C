28cb3a0f4c4ac9e764d8012bb816ce5635fd5914
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-28 03:17:05 -0700

    fram.C

---
+++ fram.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    float length, width, area;
+
+      printf("Enter length of rectangle: ");
+    scanf("%f", &length);
+    printf("Enter width of rectangle: ");
+    scanf("%d", &width);
+
+      area = length * width;
+
+        printf("Area of rectangle = %f sq. units ", area);
+
+    getch();
+}
